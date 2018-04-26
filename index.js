c3a00b24a908ef6b1a9ae2d3a94d5ad4967729b8
Jon Salmon <jonathan.salmon@hotmail.co.uk>
2018-04-26 18:03:49 +0100

    Updated from field

--- index.js
+++ index.js
@@ -37,13 +37,18 @@ app.get('/', (req, res) => res.send('Hello World'))
 app.post('/messages', (request, response) => {
   try {
     let data = JSON.parse(JSON.stringify(request.body)),
-      message = data[0].msys.relay_message.content.email_rfc822;
+      message = data[0].msys.relay_message.content.email_rfc822,
+      from = process.env.FORWARD_FROM;
+
+    if (message.match(/^From:\s?([\w\s]+)?<[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+>$/m)) {
+      from = message.match(/^From:\s?([\w\s]+)?<[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+>$/m)[1].trim() + '<' + process.env.FORWARD_FROM + '>';
+    }
 
     if (message.match(/^Reply-To: .*$/m)) {
-      message = message.replace(/^From: .*$/m, 'From: ' + process.env.FORWARD_FROM);
+      message = message.replace(/^From: .*$/m, 'From: ' + from);
     } else {
       message = message.replace(/^From: .*$/m,
-        'From: ' + process.env.FORWARD_FROM +
+        'From: ' + from +
         '\r\nReply-To: ' + data[0].msys.relay_message.friendly_from);
     }
 
@@ -61,6 +66,7 @@ app.post('/messages', (request, response) => {
     }, function(err, res) {
       if (err) {
         console.error('Transmission failed: ' + JSON.stringify(err));
+        return response.status(500).send('Transmission error');
       } else {
         console.log('Transmission succeeded: ' + JSON.stringify(res.body));
       }
