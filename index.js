73e0b8b1ae0ecfa63fc4174920cd90bfc6152117
Jon Salmon <jonathan.salmon@hotmail.co.uk>
2018-04-27 01:29:12 +0100

    Moved to new config system

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env nodejs
 
-require('dotenv').load();
-
-let q = require('q'),
+let config = require('config'),
   express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
   SparkPost = require('sparkpost'),
-  sp = new SparkPost(process.env.SPARKPOST_API_KEY);
+  sp = new SparkPost(config.get('keys.SPARKPOST_API_KEY'));
 
 app.use(bodyParser.json());
 // Default of 100k might be too small for many attachments
@@ -15,17 +13,17 @@ app.use(bodyParser.json({
   limit: '10mb'
 }));
 
-if (process.env.SPARKPOST_API_KEY === undefined) {
+if (!config.has('keys.SPARKPOST_API_KEY')) {
   console.error('SPARKPOST_API_KEY must be set');
   process.exit(1);
 }
 
-if (process.env.FORWARD_FROM === undefined) {
+if (!config.has('route.FORWARD_FROM')) {
   console.error('FORWARD_FROM must be set');
   process.exit(1);
 }
 
-if (process.env.FORWARD_TO === undefined) {
+if (!config.has('route.FORWARD_TO')) {
   console.error('FORWARD_TO must be set');
   process.exit(1);
 }
@@ -38,10 +36,15 @@ app.post('/messages', (request, response) => {
   try {
     let data = JSON.parse(JSON.stringify(request.body)),
       message = data[0].msys.relay_message.content.email_rfc822,
-      from = process.env.FORWARD_FROM;
+      token = request.get('X-MessageSystems-Webhook-Token'),
+      from = config.route.FORWARD_FROM;
+
+    if (token !== config.AUTH_TOKEN) {
+      return response.status(503).send('Unorthorised request');
+    }
 
     if (message.match(/^From:\s?([\w\s]+)?<[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+>$/m)) {
-      from = message.match(/^From:\s?([\w\s]+)?<[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+>$/m)[1].trim() + '<' + process.env.FORWARD_FROM + '>';
+      from = message.match(/^From:\s?([\w\s]+)?<[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+>$/m)[1].trim() + '<' + config.route.FORWARD_FROM + '>';
     }
 
     if (message.match(/^Reply-To: .*$/m)) {
@@ -60,7 +63,7 @@ app.post('/messages', (request, response) => {
       },
       recipients: [{
         address: {
-          email: process.env.FORWARD_TO
+          email: config.route.FORWARD_TO
         }
       }],
       open_tracking: false,
@@ -80,4 +83,4 @@ app.post('/messages', (request, response) => {
   }
 });
 
-app.listen(process.env.PORT, () => console.log('Example app listening on port ' + process.env.PORT))
+app.listen(config.get('server.PORT'), () => console.log('Example app listening on port ' + config.server.PORT))
